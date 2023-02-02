6322d3a23f8c420c1e29364971c0279bc3a98711
Tobias Marstaller <tobias.marstaller@beyonnex.io>
2023-02-02 14:57:11 +0100

    add ProgramSlotChangedMessage

--- src/main/kotlin/com/github/tmarsteel/voxamplibrarian/protocol/ProgramSlot.kt
+++ src/main/kotlin/com/github/tmarsteel/voxamplibrarian/protocol/ProgramSlot.kt
@@ -1,6 +1,6 @@
 package com.github.tmarsteel.voxamplibrarian.protocol
 
-enum class ProgramSlot(val protocolValue: Byte) {
+enum class ProgramSlot(val protocolValue: Byte) : ProtocolSerializable {
     A1(0x00),
     A2(0x01),
     A3(0x02),
@@ -9,4 +9,19 @@ enum class ProgramSlot(val protocolValue: Byte) {
     B2(0x05),
     B3(0x06),
     B4(0x07),
+    ;
+
+    override fun writeTo(out: BinaryOutput) {
+        out.write(protocolValue)
+    }
+
+    companion object {
+        fun readFrom(input: BinaryInput): ProgramSlot {
+            val value = input.nextByte()
+            return enumValues<ProgramSlot>().find { it.protocolValue == value }
+                ?: throw MessageParseException.InvalidMessage(
+                    "Unknown program slot ${value.hex()}"
+                )
+        }
+    }
 }
---
+++ src/main/kotlin/com/github/tmarsteel/voxamplibrarian/protocol/ProgramSlotChangedMessage.kt
@@ -0,0 +1,18 @@
+package com.github.tmarsteel.voxamplibrarian.protocol
+
+data class ProgramSlotChangedMessage(
+    val slot: ProgramSlot,
+) : MidiProtocolMessage {
+    override fun writeTo(out: BinaryOutput) {
+        out.write(PREFIX)
+        out.write(slot)
+    }
+
+    companion object : MidiProtocolMessage.Factory<ProgramSlotChangedMessage> {
+        val PREFIX = byteArrayOf(0x30, 0x00, 0x01, 0x34, 0x4e, 0x00)
+        override fun parse(fullMessage: BinaryInput): ProgramSlotChangedMessage {
+            requirePrefix(fullMessage, PREFIX, ProgramSlotChangedMessage::class)
+            return ProgramSlotChangedMessage(ProgramSlot.readFrom(fullMessage))
+        }
+    }
+}
