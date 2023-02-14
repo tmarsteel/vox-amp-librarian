43a52fdb6f6f01260c026434dc01f4773c656594
Tobias Marstaller <tobias.marstaller@beyonnex.io>
2023-02-14 17:34:17 +0100

    implement toString for Continuous

--- src/main/kotlin/com/github/tmarsteel/voxamplibrarian/appmodel/Continuous.kt
+++ src/main/kotlin/com/github/tmarsteel/voxamplibrarian/appmodel/Continuous.kt
@@ -8,15 +8,21 @@ sealed interface Continuous<Self : Continuous<Self>> : Comparable<Self> {
 class Frequency(val millihertz: Int) : Continuous<Frequency> {
     override val intValue get() = millihertz
 
+    override fun toString() = "$millihertz mHz"
+
     companion object {
         val Int.mHz: Frequency get() = Frequency(this)
     }
 }
 
-class UnitlessSingleDecimalPrecision(override val intValue: Int) : Continuous<UnitlessSingleDecimalPrecision>
+class UnitlessSingleDecimalPrecision(override val intValue: Int) : Continuous<UnitlessSingleDecimalPrecision> {
+    override fun toString() = intValue.toString()
+}
 
 class Duration(val milliseconds: Int) : Continuous<Duration> {
     override val intValue get() = milliseconds
+
+    override fun toString() = "$milliseconds ms"
 
     companion object {
         val Int.ms: Duration get() = Duration(this)
