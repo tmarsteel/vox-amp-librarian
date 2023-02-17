e35606a6d17b22265829321d03d460479c9ee472
Tobias Marstaller <tobias.marstaller@gmail.com>
2023-02-17 13:50:38 +0100

    model error response 0x26 and 0x24

--- src/main/kotlin/com/github/tmarsteel/voxamplibrarian/protocol/message/ErrorMessage.kt
+++ src/main/kotlin/com/github/tmarsteel/voxamplibrarian/protocol/message/ErrorMessage.kt
@@ -4,15 +4,28 @@ import com.github.tmarsteel.voxamplibrarian.BinaryInput
 import com.github.tmarsteel.voxamplibrarian.requireEOF
 import com.github.tmarsteel.voxamplibrarian.requirePrefix
 
-class ErrorMessage : MessageToHost {
+abstract class ErrorMessage(
+    val message: String,
+) : MessageToHost {
+    class GeneralError : ErrorMessage("Unknown error")
+    class InvalidValue : ErrorMessage("Invalid value")
+
+    override fun toString() = message
+
     companion object : MidiProtocolMessage.Factory<ErrorMessage> {
-        val PREFIX = byteArrayOf(0x30, 0x00, 0x01, 0x34, 0x26)
+        val PREFIX = byteArrayOf(0x30, 0x00, 0x01, 0x34)
         override val type = ErrorMessage::class
         override fun parse(fullMessage: BinaryInput): ErrorMessage {
             requirePrefix(fullMessage, PREFIX, ErrorMessage::class)
+            val indicator = fullMessage.nextByte()
+            val error = when (indicator.toInt()) {
+                0x26 -> GeneralError()
+                0x24 -> InvalidValue()
+                else -> throw MessageParseException.PrefixNotRecognized(ErrorMessage::class)
+            }
             requireEOF(fullMessage)
 
-            return ErrorMessage()
+            return error
         }
     }
 }
